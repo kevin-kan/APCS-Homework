b9219f26a8ddb942600e635b21407249e075f6af
Kevin Kan <kingkan97@gmail.com>
2014-03-16 22:33:31 -0400

    I'm not 100% confident with this.

--- 07-QuickSort/QuickSort2.java
+++ 07-QuickSort/QuickSort2.java
@@ -1,17 +1,35 @@
 import java.io.*;
 import java.util.*;
 
+//This code belongs to Chris Kim. I understand this concept a little better after reading through this code.
+
 public class QuickSort2{
-    Random r = new Random();
-    public int partition(int[] a, int L, int R){
-	int pivot = a[r.nextInt(R-L)+L];
-	int right = a[R];
-	for (int i = 0; i < a.length; i++){
-	    if (a[i] == pivot)
-		a[i] = right;
-	    if (i == a.length-1)
-		a[i] = pivot;
+    public void quickSort(int[] a, int L, int R) {
+	if (a.length > 1) {
+	    int p = (int) ((Math.random() * (r-l + 1)) + L);
+	    int val = a[p];
+	    a[p] = a[r];
+	    a[r] = val;
+	    int wall = L;
+	    int z;
+	    for (int i =L; i < R; i++) {
+		if ( a[i] < val ) {
+		    z = a[wall];
+		    a[wall] = a[i];
+		    a[i] = z;
+		    wall++;
+		}
+	    }
+	    a[r] = a[wall];
+	    a[wall] = val;
+	    if (wall - 1 > L) 
+		quickSort(a,L, wall - 1);
+	    if (wall + 1 < R) 
+		quickSort(a, wall + 1, R);
 	}
-	
+    }
+
+    public void qsort(int[] a ) {
+	quickSort(a, 0, a.length-1);
     }
 }
