bda3f22824f8409848c3b0ca9f78d45ea63cd7af
Kevin Kan <kingkan97@gmail.com>
2014-03-19 19:38:44 -0400

    String --> Node Problems

--- 08-LinkedList/MyListedList.java
+++ 08-LinkedList/MyListedList.java
@@ -1,45 +1,51 @@
 public class MyLinkedList{
     private Node head;
-    
+    /*@@@@@@@@@@@@@@@
+      HOW DO YOU
+      REFERENCE A NODE 
+      BY ITS STRING EASILY?
+    @@@@@@@@@@@@@@@*/   
     public MyLinkedList(){
 	head = null;
-    }
-    
-    public void add(String s){
-	Node tmp = new Node(s);
-	//head = tmp;
-	tmp.setNext(head);
-	head = tmp;
     }
     
     public String toString(){
 	Node temp = head;
 	String s = "";
 	while (temp != null){
-	    s = s + temp;
+	    s = s + temp.toString();
 	    temp = temp.getNext();
 	}
 	return s;
     }
-    /* ---------incomplete--------
+
+    public void add(String s){
+	Node temp = new Node(s);
+	//head = temp;
+	temp.setNext(head);
+	head = temp;
+    }
+       
     public void add(int i, String s){
 	Node temp = new Node(s);
-	temp.setNext(
-
+	temp.setNext(get(i+1));
+	get(i-1).setNext(temp);
     }
-    */
+    
     public String get(int i){
 	Node temp = head;
 	for (int j = 0; j< length();j++){
-	    if (temp = i)
+	    if (temp == i)
 		return j;
 	    temp = temp.getNext();
 	}
     }
+
     public String set(int i, String s){
 	add(i, s);
 	remove(i+1);
     }
+
     public String remove(int i){
 	get(i - 1).setNext(get(i + 1));
 	get(i).setNext(null);
@@ -49,7 +55,7 @@ public class MyLinkedList{
 	Node temp = head;
 	int count = 0;
 	while (temp != null){
-	    if (temp == s)
+	    if (temp.toString() == s)
 		return count;
 	    count = count +1;
 	}
