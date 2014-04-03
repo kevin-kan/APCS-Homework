b7b5664a7612e2b9a17089c0bc6628f03071ffef
Kevin Kan <kingkan97@gmail.com>
2014-04-03 09:23:23 -0400

    start of Stack

---
+++ 12-Stack/Stack.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Stack{
+	public Stack(){
+		top = null;
+	}
+	public void push(String s){
+
+	}
+	public String pop(){
+	
+	}
+	public String peek(){
+		String x = pop();
+		push(x);
+	}
+	
+}
